8deb959a8e32e223d8512f2841bac9f786230326
AdwithGowda <126180447+AdwithGowda@users.noreply.github.com>
2026-02-04 13:39:09 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://finance-366p.onrender.com/expenses';
+const API_URL = 'https://finance-lz2e.onrender.com';
 
 export const fetchExpenses = () => axios.get(`${API_URL}/expenses`);
 export const createExpense = (data) => axios.post(`${API_URL}/expenses`, data);
