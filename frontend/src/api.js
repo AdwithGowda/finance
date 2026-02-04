1a93e72631d0edd162d51a3baad178d6c4d1ce51
AdwithGowda <126180447+AdwithGowda@users.noreply.github.com>
2026-02-04 12:19:41 +0530

    Update API_URL to new endpoint

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://finance-2-t4il.onrender.com';
+const API_URL = 'https://finance-366p.onrender.com';
 
 export const fetchExpenses = () => axios.get(`${API_URL}/expenses`);
 export const createExpense = (data) => axios.post(`${API_URL}/expenses`, data);
