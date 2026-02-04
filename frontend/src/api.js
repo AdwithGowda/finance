6542eb6f9a2a2dcf012ab4c986b068453c3a543b
AdwithGowda <126180447+AdwithGowda@users.noreply.github.com>
2026-02-04 12:03:21 +0530

    Change API URL to production endpoint

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000';
+const API_URL = 'https://finance-2-t4il.onrender.com';
 
 export const fetchExpenses = () => axios.get(`${API_URL}/expenses`);
 export const createExpense = (data) => axios.post(`${API_URL}/expenses`, data);
